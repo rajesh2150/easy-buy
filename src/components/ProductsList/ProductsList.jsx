941e37bd6d4f65e3d5405f40d6aa642aa956039d
Rajesh Korlapati <rajeshkorlapati465@gmail.com>
2024-01-31 16:56:19 +0530

    feat: adding a carosul to web page

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -10,7 +10,7 @@ const ProductsList = () => {
     axios
       .get("https://fakestoreapi.com/products")
       .then((res) => setProducts(res.data));
-    console.log(Products);
+  
   }, []);
 
   const history = useNavigate(); //history variable
@@ -20,24 +20,47 @@ const ProductsList = () => {
     console.log(Product)
     history(`/details/${Product.id}`);
   };
-  return (<>
-  <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
- <img  className="add-banner" src={"https://mydailybells.com.ng/businessbells/wp-content/uploads/sites/3/2021/07/naija-shopping.jpg"}/>
-  </div>
-    <div className="product-div1">
-      {Products.map((Product) => (
-        <>
-          <div className="Product-List-Div1">
+
+  const [img, setImg] = useState("https://m.media-amazon.com/images/G/31/img21/MA2023/Winterflip/P0/winter/hero/new/USPA_3000x8001._SX3000_QL85_FMpng_.png");
+  const imagesArray = [
+    "https://m.media-amazon.com/images/G/31/img21/MA2023/Winterflip/P0/winter/hero/Van_heusen_3000x800._SX3000_QL85_FMpng_.png",
+    "https://m.media-amazon.com/images/G/31/img21/MA2023/AFrevamp_winterflip/Menhero/Wimter_bestseller_3000x800._SX3000_QL85_FMpng_.png",
+    "https://m.media-amazon.com/images/G/31/img21/MA2023/Winterflip/P0/winter/hero/new/USPA_3000x8001._SX3000_QL85_FMpng_.png"
+  ];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % imagesArray.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [imagesArray]);
+
+  useEffect(() => {
+    setImg(imagesArray[index]);
+  }, [index, imagesArray]);
+
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <img className="add-banner" src={img} alt="Banner" />
+      </div>
+      <div className="product-div1">
+        {Products.map((Product) => (
+          <div className="Product-List-Div1" key={Product.id}>
             <div className="product-image-div1">
               <img src={Product.image} alt="product" className="product-image1" />
             </div>
             <div className="product-description-div1">
-              <p>{Product.title.substring(0,4)}...</p>
+              <p>{Product.title.substring(0, 4)}...</p>
               <span className="product-rating1">
-            {Product?.rating && Product?.rating.rate}
-            <IoMdStarOutline size={12} />
-          </span>
-              <p className="product-price1">${Product.price}<s className="product-strike-price1">${Product.price*2}</s></p>
+                {Product?.rating && Product?.rating.rate}
+                <IoMdStarOutline size={12} />
+              </span>
+              <p className="product-price1">${Product.price}<s className="product-strike-price1">${Product.price * 2}</s></p>
               <button
                 onClick={() => handleAddtoCart(Product)}
                 className="add-to-cart-button1">
@@ -45,9 +68,8 @@ const ProductsList = () => {
               </button>
             </div>
           </div>
-        </>
-      ))}
-    </div>
+        ))}
+      </div>
     </>
   );
 };
