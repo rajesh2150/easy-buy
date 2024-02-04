c2e30af637df5e8638ff2c9879faa31c60ddcff7
Rajesh Korlapati <rajeshkorlapati465@gmail.com>
2024-02-04 11:38:00 +0530

    feat: Add Routing for cart to buy/place order

--- src/components/Navbar/Cart/Cart.jsx
+++ src/components/Navbar/Cart/Cart.jsx
@@ -1,53 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cart.css";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromCart } from "../../../Store/createSlice";
-import {FaArrowLeft} from 'react-icons/fa';
-import {useNavigate} from 'react-router-dom'
+import { FaArrowLeft } from "react-icons/fa";
+import { useNavigate, useParams } from "react-router-dom";
+import { IoMdStarOutline } from "react-icons/io";
+import { buynow } from "../../../Store/orderSlice";
+import EmptyCart from "./EmptyCart";
+
 const Orders = () => {
-  const history = useNavigate()
+  const {id}=useParams()
+  const history = useNavigate();
   const dispatch = useDispatch();
   const cartProducts = useSelector((state) => state.cart);
+  const [prices, setPrices] = useState(
+    cartProducts.map((product) => product.price)
+  );
+
   const handleRemove = (id) => {
     dispatch(removeFromCart(id));
   };
-  const handleHome=()=>{
-    history('/')
 
+  const handleHome = () => {
+    history('/');
+  };
+  var count = 1;
+  const handleIncrement = (index, price) => {
+    count += 1;
+    setPrices((prevPrices) => {
+      const newPrices = [...prevPrices];
+      newPrices[index] = (newPrices[index] || 0) + price;
+      return newPrices;
+    });
+  };
+  const handleDecrement = (index, price) => {
+    count -= 1;
+    setPrices((prevPrices) => {
+      const newPrices = [...prevPrices];
+      newPrices[index] = (newPrices[index] || 0) - price;
+      return newPrices;
+    });
+  };
+  const handlePlaceOrder=(p)=>{
+    dispatch(buynow(p));
+    dispatch(removeFromCart(p.id));
+    history('/Orders')
   }
 
+  console.log(cartProducts.length)
   return (
     <>
-    <button className="left-arrow-button" onClick={() => handleHome()}>
-        <FaArrowLeft size={20} />
+      <button className="left-arrow-button" onClick={() => handleHome()}>
+        <FaArrowLeft className="left-arrow" size={20} />
       </button>
       <div className="cart-div">
         <h2>Cart</h2>
         <div className="product-div">
-        {cartProducts.map((Product) => (
-          <div className="Product-List-Div1 column-div " key={Product.id}>
-            <div className="product-image-div1">
-              <img src={Product.image} alt="product" className="product-image1 cart-image" />
+          {cartProducts.length>=1 ?cartProducts.map((Product, index) => (
+            <div className="Product-List-Div1 column-div " key={Product.id}>
+              <div className="product-image-div1 product-image-cart">
+                <img
+                  src={Product.image}
+                  alt="product"
+                  className="product-image1 cart-image"
+                />
+                <div className="inc-dec-buttons-div">
+                  <button onClick={() => handleIncrement(index, Product.price)}>
+                    +
+                  </button>
+                  <p>{count ? count : 1}</p>
+                  <button
+                    disabled={Product.price >= prices[index] ? true : false}
+                    onClick={() => handleDecrement(index, Product.price)}>
+                    -
+                  </button>
+                </div>
+              </div>
+              <div className="product-description-div1 product-description-div-cart">
+                <p>{Product.title}</p>
+                <span className="product-rating1 product-rating-cart ">
+                  {Product?.rating && Product?.rating.rate}
+                  <IoMdStarOutline size={12} />
+                </span>
+
+                <p className="product-price1" id="price">
+                  ${prices[index] ? prices[index] : Product.price}
+                </p>
+                <div style={{margin:0,marginBottom:5}}>
+                  <s className="product-strike-price1" style={{margin:0}}>
+                    ${prices[index] ? prices[index] * 2 : Product.price * 2}
+                  </s>
+                  <span
+                    style={{
+                      color: "green",
+                      display: "inline",
+                      fontSize: 15,
+                      margin: 3,
+                    }}>
+                    {" "}
+                    50% off
+                  </span>
+                </div>
+
+                <button
+                  onClick={() => handleRemove(Product.id)}
+                  className="add-to-cart-button1 remove-button-cart">
+                  Remove
+                </button>
+                <button className="place-order" onClick={()=>{handlePlaceOrder(Product)}}>Place Order</button>
+              </div>
             </div>
-            <div className="product-description-div1">
-              <p>{Product.title}</p>
-              <span className="product-rating1">
-                {Product?.rating && Product?.rating.rate}
-                {/* <IoMdStarOutline size={12} /> */}
-              </span>
-              <p className="product-price1">${Product.price}<s className="product-strike-price1">${Product.price * 2}</s></p>
-              <button
-                onClick={() => handleRemove(Product.id)}
-                className="add-to-cart-button1">
-                Remove
-              </button>
-            </div>
-          </div>
-        ))}
+          )):<EmptyCart/>}
         </div>
       </div>
     </>
   );
-};
+                  }
 
 export default Orders;
